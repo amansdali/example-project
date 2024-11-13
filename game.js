97823b57cb00df484f18322d3e461d9770e71a7e
Amanda <77864882+amansdali@users.noreply.github.com>
2024-11-12 23:16:45 -0500

    Update game.js

--- game.js
+++ game.js
@@ -6,6 +6,37 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 const ballRadius = 10;
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = true;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = true;
+    } else if (e.key === 'Up' || e.key === 'ArrowUp') {
+        upPressed = true;
+    } else if (e.key === 'Down' || e.key === 'ArrowDown') {
+        downPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = false;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = false;
+    } else if (e.key === 'Up' || e.key === 'ArrowUp') {
+        upPressed = false;
+    } else if (e.key === 'Down' || e.key === 'ArrowDown') {
+        downPressed = false;
+    }
+}
 
 function drawBall() {
     context.beginPath();
@@ -18,12 +49,23 @@ function drawBall() {
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    x += dx;
-    y += dy;
+
+    if (rightPressed) {
+        x += dx;
+    }
+    if (leftPressed) {
+        x -= dx;
+    }
+    if (upPressed) {
+        y -= dy;
+    }
+    if (downPressed) {
+        y += dy;
+    }
 
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
-}
+    }
     if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
